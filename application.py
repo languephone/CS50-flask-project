f9653cdb9a94fc91d6155f6fa75f9754d9471220
Daniel Shapiro <73309519+languephone@users.noreply.github.com>
2021-04-05 15:10:33 +0000

    Made quote route compliant with check50.

--- application.py
+++ application.py
@@ -173,7 +173,7 @@ def logout():
 def quote():
     """Get stock quote."""
     if request.method == "GET":
-        return render_template("quote_live.html", )
+        return render_template("quote.html", )
     else:
         # Ensure symbol was submitted
         if not request.form.get("symbol"):
@@ -183,8 +183,8 @@ def quote():
             return apology("symbol not recognized", 400)
 
         quote = lookup(request.form.get("symbol"))
-        # return render_template("quote_live.html", quote=quote, price=quote['price'])
-        return jsonify(quote)
+        return render_template("quote.html", quote=quote)
+        #return jsonify(quote)
 
 
 @app.route("/register", methods=["GET", "POST"])
